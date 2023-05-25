2dd7ab6b5f58b4f1f423813b57d00da4be4722a6
Mehmet Kutlu <mehmetkutlu512@gmail.com>
2023-05-25 23:55:11 +0300

    22.05.2023

--- src/test/java/get_requests/Get02.java
+++ src/test/java/get_requests/Get02.java
@@ -1,5 +1,51 @@
 package get_requests;
 
-public class Get02 {
+import io.restassured.response.Response;
+import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Get02 {
+            /*
+       Given
+           https://restful-booker.herokuapp.com/booking/0
+       When
+           User send a GET Request to the url
+       Then
+           HTTP Status code should be 404
+       And
+           Status Line should be "HTTP/1.1 404 Not Found"
+       And
+           Response body contains "Not Found"
+       And
+           Response body does not contain "TechProEd"
+       And
+           Server is "Cowboy"
+    */
+
+    @Test
+    public void get02() {
+        //Set the url
+        String url = "https://restful-booker.herokuapp.com/booking/0";
+
+        //Set the expected data
+
+        //Send the request and get the response
+        Response response = given().get(url);
+        response.prettyPrint();
+
+        //Do assertion
+        response.then().statusCode(404).//HTTP Status code should be 404
+                statusLine("HTTP/1.1 404 Not Found");//Status Line should be "HTTP/1.1 404 Not Found"
+
+        //Response body contains "Not Found"
+        //System.out.println("-->" + response.asString());
+        assertTrue(response.asString().contains("Not Found"));
+
+        //Response body does not contain "TechProEd"
+        assertFalse(response.asString().contains("TechProEd"));
+
+    }
 }
