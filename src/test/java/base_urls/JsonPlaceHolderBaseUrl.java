7cabcf4e171632a2c47013c94d44571e937ce7b9
Mehmet Kutlu <mehmetkutlu512@gmail.com>
2023-05-26 18:30:00 +0300

    22.05.2023

---
+++ src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
@@ -0,0 +1,19 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class JsonPlaceHolderBaseUrl {
+    //setUp() methodu ile request işleminde tekrarlı şekilde yapılacak işlemler burada bir kez yapılacak.
+    //@Test öncesi çalışması için @Before annotation u ekliyoruz.
+    protected RequestSpecification spec;
+
+    @Before //Her @Test methodu öncesi çalışır.
+    public void setUp()  {
+        spec = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com/").setAccept(ContentType.JSON).build();
+
+
+    }
+}
