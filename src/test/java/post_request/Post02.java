e2149c5dc0fc76214d29e6af4419b6ac6528f56a
Mehmet Kutlu <mehmetkutlu512@gmail.com>
2023-05-29 12:49:55 +0300

    29.05.2023

--- src/test/java/post_request/Post02.java
+++ src/test/java/post_request/Post02.java
@@ -1,4 +1,55 @@
 package post_request;
 
-public class Post02 {
+import base_urls.HerOkuAppBaseUrl;
+import org.junit.Test;
+import test_data.HerOkuAppTestData;
+
+import java.util.Map;
+
+public class Post02 extends HerOkuAppBaseUrl {
+           /*
+   Given
+       1) https://restful-booker.herokuapp.com/booking
+       2) {
+            "firstname": "John",
+            "lastname": "Doe",
+            "totalprice": 11111,
+            "depositpaid": true,
+            "bookingdates": {
+                "checkin": "2021-09-09",
+                "checkout": "2021-09-21"
+             }
+          }
+    When
+       I send POST Request to the Url
+    Then
+       Status code is 200
+       And response body should be like {
+                                           "bookingid": 5315,
+                                           "booking": {
+                                               "firstname": "John",
+                                               "lastname": "Doe",
+                                               "totalprice": 11111,
+                                               "depositpaid": true,
+                                               "bookingdates": {
+                                                   "checkin": "2021-09-09",
+                                                   "checkout": "2021-09-21"
+                                               }
+                                           }
+                                        }
+    */
+
+    @Test
+    public void post02() {
+        //set the url
+        spec.pathParam("first", "booking");
+
+        //set the expected data
+        HerOkuAppTestData obj = new HerOkuAppTestData();
+        Map<String, String> bookingdatesMap =  obj.bookingdatesMapMethod("2021-09-09","2021-09-21");
+        Map<String, Object> expectedData =  obj.expectedDataMethod("John", "Doe", 11111, true, bookingdatesMap, "");
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the request and get the response
+    }
 }
