1c9c7ca01c607257d93ba9b4bb06c500f120c235
Mehmet Kutlu <mehmetkutlu512@gmail.com>
2023-06-10 13:41:23 +0300

    10.06.2023

--- src/test/java/base_urls/GmiBankBaseUrl.java
+++ src/test/java/base_urls/GmiBankBaseUrl.java
@@ -5,12 +5,32 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
+import static utils.AuthenticationGmiBank.generateToken;
+
 public class GmiBankBaseUrl {
 
     protected RequestSpecification spec;
 
     @Before
     public void setUp()  {
-        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).addHeader("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJrX3R3YWluIiwiYXV0aCI6IlJPTEVfQURNSU4sUk9MRV9NQU5BR0VSIiwiZXhwIjoxNjg2MTU5OTQwfQ.0b_sKSyzaHJiYZWQOIVSi4xFZbtuCCWr4m3RYQYy3T4V1WlGFQ42GW2omCyRDERNq4m4AozN87kdSk22fnoQzw").setBaseUri("https://gmibank.com/").build();
+        spec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", "Bearer "+generateToken())
+                .setBaseUri("https://gmibank.com/")
+                .build();
     }
 }
+
+//https://app.swaggerhub.com./apis/yasinaniltechpro/GmiBank/0.0.1#/ -->Swagger dökümanı
+
+//jwt --> json web token
+
+//basic aut (username ve password olacak) veya Bearer token ile authorization yapabiliriz
+
+/*
+{
+    "password":"Mark.123",
+    "rememberMe":true,
+    "username":"mark_twain"
+}
+ */
